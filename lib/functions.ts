b2771d8ad884e2a4f371f860973578564d1e5188
sharbl4 <antonnedelko@outlook.com>
2024-12-07 17:18:30 +0600

    Update functions.ts

--- lib/functions.ts
+++ lib/functions.ts
@@ -8,11 +8,6 @@ export async function reviewProfile(ctx: MyContext) {
   await ctx.reply(
     `${info.name}, ${info.age}\n` +
       `Список интересов: ${info.interests.toString()}`,
-  );
-  await ctx.reply("Геопозиция района, где будет удообно встретиться:");
-  await ctx.replyWithLocation(info.geo.latitude, info.geo.longitiute);
-  await ctx.reply("Все верно?", {
-    reply_markup: acceptKeyboard,
   });
 }
 
