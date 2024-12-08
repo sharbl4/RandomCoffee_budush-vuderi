858fd97ba4d5571c786c221bf8236de4435cd5b9
NPr0ject <iwanow32855@gmail.com>
2024-12-08 20:19:19 +0300

    debag&

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -3,11 +3,13 @@ import { Context } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
 export interface UserInfo {
   id: number;
   name: string;
-  age: string;
+  age: number;
   interests: string;
   coffee:string;
   done: boolean;
   state: string;
+  time: string;
+  rating: number;
 }
 
 
