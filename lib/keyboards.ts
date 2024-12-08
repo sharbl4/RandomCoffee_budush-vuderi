a5fef41b1d6ecfae2c746ac4e0152d6e4cb9a5eb
sharbl4 <antonnedelko@outlook.com>
2024-12-08 22:38:39 +0600

    Update keyboards.ts

--- lib/keyboards.ts
+++ lib/keyboards.ts
@@ -21,22 +21,19 @@ export const yesOrNo = new InlineKeyboard().text("Да✅", "interestsDone").tex
 
 export const menuKeyboard = new Keyboard().text("Мой профиль 👤");
 
-export const coffekeyboard = new Keyboard()
+export const interesKeyboard = new InlineKeyboard()
   .text("Скуратов. 70 лет Октября, 7").row()
   .text("Скуратов. Мира, 7А").row()
   .text("Скуратов. Красный Путь, 63").row()
   .text("Скуратов. Иртышская Набережная, 30").row()
-  .text("Энитайм. Лобкова, 6/1")
-  .oneTime()
-  .resized();
+  .text("Энитайм. Лобкова, 6/1");
 
-export const interesKeyboard=new Keyboard()
+
+export const interesKeyboard=new InlineKeyboard()
   .text("Codding").row()
   .text("Web-desigm").row()
   .text("SMM").row()
   .text("UX-design").row()
   .text("UI-design").row()
   .text("PC-building").row()
-  .text("PC-repair")
-  .oneTime()
-  .resized();
+  .text("PC-repair");
