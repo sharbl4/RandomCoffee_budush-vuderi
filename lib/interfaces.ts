1c0036031161fb6b68fc892e81bd76e5abdac63a
NPr0ject <iwanow32855@gmail.com>
2024-12-08 18:56:21 +0300

    age to string

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -3,7 +3,7 @@ import { Context } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
 export interface UserInfo {
   id: number;
   name: string;
-  age: number;
+  age: string;
   interests: string;
   coffee:string;
   done: boolean;
