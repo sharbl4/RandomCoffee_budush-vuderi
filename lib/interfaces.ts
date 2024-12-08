b42047a34abd15ddca1c91f89ef45eb0b7896dbf
sharbl4 <antonnedelko@outlook.com>
2024-12-08 21:51:45 +0600

    Update interfaces.ts

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -3,7 +3,7 @@ import { Context } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
 export interface UserInfo {
   id: number;
   name: string;
-  age: string;
+  age: number;
   interests: string;
   coffee:string;
   done: boolean;
